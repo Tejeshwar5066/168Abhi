102f224ebd3a6644e30b2e311d4a48d5f09ed66e
Abhishek shukla <91980653+168Abhi@users.noreply.github.com>
2024-08-09 14:54:08 +0530

    Delete Student_Management_System-restapi directory

--- Student_Management_System-restapi/src/main/java/com/cdac/training/pms/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cdac.training.pms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cdac.training.pms.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
